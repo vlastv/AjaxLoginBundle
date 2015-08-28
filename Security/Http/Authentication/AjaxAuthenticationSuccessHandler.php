c1b7dd98ee998b1814c1edea63ae23e40da06a67
Vladislav Vlastovskiy <me@vlastv.ru>
2015-08-29 01:06:26 +0300

    Fix target path
    Symfony allow use route name instead relative path as default_target_path

--- Security/Http/Authentication/AjaxAuthenticationSuccessHandler.php
+++ Security/Http/Authentication/AjaxAuthenticationSuccessHandler.php
@@ -2,8 +2,8 @@
 
 namespace Divi\AjaxLoginBundle\Security\Http\Authentication;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationSuccessHandler;
 
@@ -18,10 +18,10 @@ class AjaxAuthenticationSuccessHandler extends DefaultAuthenticationSuccessHandl
     public function onAuthenticationSuccess(Request $request, TokenInterface $token)
     {
         if ($request->isXmlHttpRequest()) {
-            return new Response(json_encode(array(
+            return new JsonResponse((array(
                 'has_error'   => false,
                 'username'    => $token->getUser()->getUsername(),
-                'target_path' => $this->determineTargetUrl($request)
+                'target_path' => $this->httpUtils->generateUri($request, $this->determineTargetUrl($request))
             )));
         }
 
